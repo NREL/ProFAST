98c9acc55f68cd756072f37e7bc963c43dc44523
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2025-07-30 14:48:11 -0600

    added more functions to utilities

--- ProFAST/tools/utilities.py
+++ ProFAST/tools/utilities.py
@@ -31,4 +31,45 @@ def write_yaml(filename,data):
 
     with open(filename, 'w+') as file:
         yaml.dump(data, file,sort_keys=False,encoding = None,default_flow_style=False)
-        
+
+def update_keyname(orig_dict, init_key, new_keyname):
+
+    for key, val in orig_dict.copy().items():
+        if isinstance(val, dict):
+            tmp = update_keyname(orig_dict.get(key, {}), init_key, new_keyname)
+            orig_dict[key] = tmp
+        else:
+            if isinstance(key, list):
+                for i, k in enumerate(key):
+                    if k == init_key:
+                        orig_dict.update({new_keyname:orig_dict.get(k)})
+                    else:
+                        orig_dict[k] = orig_dict.get(key, []) + val[i]
+            elif isinstance(key, str):
+                if key == init_key:
+                    orig_dict.update({new_keyname:orig_dict.get(key)})
+    return orig_dict
+
+def remove_keynames(orig_dict, init_key):
+
+    for key, val in orig_dict.copy().items():
+        if isinstance(val, dict):
+            tmp = remove_keynames(orig_dict.get(key, {}), init_key)
+            orig_dict[key] = tmp
+        else:
+            if isinstance(key, list):
+                for i, k in enumerate(key):
+                    if k == init_key:
+                        orig_dict.pop(k)
+                    else:
+                        orig_dict[k] = orig_dict.get(key, []) + val[i]
+            elif isinstance(key, str):
+                if key == init_key:
+                    orig_dict.pop(key)
+    return orig_dict
+
+def rename_dict_keys(input_dict,init_keyname,new_keyname):
+    input_dict = update_keyname(input_dict,init_keyname,new_keyname)
+    input_dict = remove_keynames(input_dict,init_keyname)
+    return input_dict
+    
